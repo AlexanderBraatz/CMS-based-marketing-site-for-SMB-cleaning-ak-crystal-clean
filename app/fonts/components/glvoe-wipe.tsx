ae1484194b0073bb139730626c323c840def7055
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-03 09:46:28 +0100

    feat:me: 1 starting on scroll animation by copying code over from old project and refamiliarising myself with the library and imprtantly allowing me to think through the problem of what actually needs to happen for this animation

--- app/fonts/components/glvoe-wipe.tsx
+++ app/fonts/components/glvoe-wipe.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
 import glove from '@/public/images/glove-e8e8e8-part-background.png';
 import Image from 'next/image';
+import { MotionValue, useScroll, useTransform, motion } from 'motion/react';
 
 export default function GloveWipe() {
+  const bigRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: bigRef,
+    offset: ['start start', 'end end'],
+  });
+
+  const range = [0, 1];
+  const targetY = -500;
+  const targetX = 900;
+  const topGlove = useTransform(scrollYProgress, range, [250, targetY]);
+  const leftGlove = useTransform(scrollYProgress, range, [250, targetX]);
+
   return (
-    <div className="relative -z-10">
+    <div ref={bigRef} className="relative -z-10">
       <div className="bg-theme-background absolute left-[50%] -mt-30 h-[164px] w-[2000px] translate-x-[-50%] -rotate-14"></div>
-      <div className="absolute left-[50%] -mt-62 flex translate-x-[-50%]">
+      <motion.div
+        style={{ top: topGlove, left: leftGlove }}
+        className="absolute left-[50%] -mt-62 flex translate-x-[-50%] bg-red-400"
+      >
         <div className="bg-theme-background2 -mr-15 h-[413px] w-[1200px]"></div>
         <div className="w-[460px]">
           <Image src={glove} alt="gloved hand " />
         </div>
         <div className="h-[413px] w-[400px]"></div>
-      </div>
+      </motion.div>
     </div>
   );
 }
