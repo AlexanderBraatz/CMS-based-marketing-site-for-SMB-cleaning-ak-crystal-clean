93080131f78df566ed037d04720a3b87a0efcf97
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-04 17:14:58 +0100

    feat:i: add rotation to whiping hand

--- components/glvoe-wipe.tsx
+++ components/glvoe-wipe.tsx
@@ -27,15 +27,16 @@ export default function GloveWipe() {
   const easeIn = (t: number) => t * t;
   const x = useTransform(scrollYProgress, (p) => easeIn(p) * xTravel);
   const y = useTransform(scrollYProgress, (p) => easeIn(p) * yTravel);
+  const rotation = useTransform(scrollYProgress, [0, 1], [10, -20]);
 
   return (
     <div ref={bigRef} className="relative -z-10 -mb-120 min-h-[500px]">
       <div className="bg-theme-background absolute left-[50%] -mt-30 h-[164px] w-[2000px] translate-x-[-50%] -rotate-14"></div>
       <motion.div ref={gloveRef} style={{ x, y }} className="absolute left-[50%] -mt-62 flex translate-x-[-50%]">
         <div className="bg-theme-background2 -mr-15 h-[413px] w-[1200px]"></div>
-        <div className="w-[460px]">
+        <motion.div style={{ rotate: rotation }} className="w-[460px]">
           <Image src={glove} alt="gloved hand " />
-        </div>
+        </motion.div>
         <div className="h-[413px] w-[400px]"></div>
       </motion.div>
     </div>
