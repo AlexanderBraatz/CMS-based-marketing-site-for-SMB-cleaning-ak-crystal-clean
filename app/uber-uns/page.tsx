006ee242373e987e426bf7d9871b18b48ef9678e
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-07 12:26:10 +0100

    feat:i: add managment image gallery

--- app/uber-uns/page.tsx
+++ app/uber-uns/page.tsx
@@ -3,6 +3,7 @@ import WaveBackground from '@/components/wave-background';
 import React from 'react';
 import heroImage from '@/public/images/team-group-outside3.jpg';
 import SomeText from '@/components/some-text';
+import ManagementGallery from '@/components/management-gallery';
 
 export default function UberUns() {
   return (
@@ -15,13 +16,13 @@ export default function UberUns() {
             heading="Ihr Partner in Gründau für Gebeudereinigung"
             subHeading="Egal ob Fassaden- oder Innenreinigung, Wir sind Ihr zuverlässiger Partner"
           />
+          <SomeText
+            text={{
+              body: 'Wer Freude an der Arbeit hat, macht sie besser. Deshalb investieren wir bei AK Crystal Clean in unser Team: regelmäßige Weiterbildungen, ein wertschätzendes Miteinander und eine Unternehmenskultur, die Menschen langfristig bindet. \n Das Ergebnis ist ein engagiertes, stolzes Team — das Ihre Räume so behandelt, als wären es die eigenen.',
+            }}
+          />
+          <ManagementGallery />
         </div>
-        <SomeText
-          text={{
-            body: 'Wer Freude an der Arbeit hat, macht sie besser. Deshalb investieren wir bei AK Crystal Clean in unser Team: regelmäßige Weiterbildungen, ein wertschätzendes Miteinander und eine Unternehmenskultur, die Menschen langfristig bindet. \n Das Ergebnis ist ein engagiertes, stolzes Team — das Ihre Räume so behandelt, als wären es die eigenen.',
-          }}
-        />
-        {/* <ManagmentGallery /> */}
       </div>
     </main>
   );
---
+++ components/management-gallery.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import TallImageDrop from './utility-components/image-tall-drop-left';
+import imagge1 from '@/public/images/team-tall-mehmet.jpg';
+import imagge2 from '@/public/images/team-tall-olga.jpg';
+import imagge3 from '@/public/images/team-tall-leo.jpg';
+import imagge4 from '@/public/images/team-tall-reiner.jpg';
+
+export default function ManagementGallery() {
+  return (
+    <div className="px-[5%]">
+      <h3 className="font-cooper-hewitt mb-5 text-[32px] leading-tight font-semibold tracking-tight opacity-80">
+        Management
+      </h3>
+
+      <div className="flex flex-row gap-5">
+        <div className="flex flex-col gap-5">
+          <TallImageDrop src={imagge1} dropIsOnLeft={true} />
+          <div className="flex flex-col gap-1">
+            <p className="font-instrument-sans text-xl font-semibold">Mehmet Akca</p>
+            <p className="font-instrument-sans tracking-tight">Geschäftsführer</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <TallImageDrop src={imagge2} dropIsOnLeft={true} />
+          <div className="flex flex-col gap-1">
+            <p className="font-instrument-sans text-xl font-semibold">Olga Akca-Klug</p>
+            <p className="font-instrument-sans tracking-tight">Personalmanagement</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <TallImageDrop src={imagge3} dropIsOnLeft={true} />
+          <div className="flex flex-col gap-1">
+            <p className="font-instrument-sans text-xl font-semibold">Leo Klug</p>
+            <p className="font-instrument-sans tracking-tight">Bereichsleitung Glasreinigung</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <TallImageDrop src={imagge4} dropIsOnLeft={true} />
+          <div className="flex flex-col gap-1">
+            <p className="font-instrument-sans text-xl font-semibold">Reiner Klix</p>
+            <p className="font-instrument-sans tracking-tight">Gebäudereinigungsmeister</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
