d234075207b8f4416b03317bbc829544afd11db0
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-09 12:10:53 +0100

    fix:i: cropwave background on extra large screens to hide shape of svg, eddeting svg would be better but this was faster

--- components/wave-background.tsx
+++ components/wave-background.tsx
@@ -5,8 +5,10 @@ import waveWide from '@/public/images/wave-hero-figma-export-wide9.svg';
 
 export default function WaveBackground() {
   return (
-    <div className="absolute top-[-450px] left-1/2 z-10 h-[340px] w-[2237px] -translate-x-1/2 bg-red-500">
-      <Image src={waveWide} alt="background" className="h-[885px] w-[2237px]" />
+    <div className="absolute top-[-450px] left-1/2 z-10 h-[340px] w-[2100px] -translate-x-1/2 overflow-x-clip">
+      <div className="absolute left-1/2 z-10 h-[340px] w-[2237px] -translate-x-1/2">
+        <Image src={waveWide} alt="background" className="h-[885px] w-[2237px]" />
+      </div>
     </div>
   );
 }
