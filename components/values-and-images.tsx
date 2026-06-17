acaa01811318673733d149329185a8963d78cc0b
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-17 15:48:56 +0100

    add heading to values section

--- components/values-and-images.tsx
+++ components/values-and-images.tsx
@@ -16,38 +16,46 @@ import icon4 from '@/public/images/icon-shield_person.svg';
 
 export default function ValuesAndImages() {
   return (
-    <motion.div {...fadeInUp} className="xs:grid-cols-[1fr_2fr] grid gap-5 px-[5%] pt-8 lg:grid-cols-4">
-      <TallImageDrop className="xs:justify-self-end order-1 justify-self-center lg:order-0" src={image1} />
-      <TallTextBox
-        className="xs:[&>p]:block order-2 justify-self-center lg:-top-8 lg:order-0"
-        icon={icon1}
-        heading="Partnerschaft"
-        text="Langfristige Partner
+    <motion.div {...fadeInUp} className="px-[5%]">
+      <p className="xs:w-full font-barlow-semi-condensed text-theme-text-highlight xs:mx-0 mx-auto w-[226px] pb-5 font-bold">
+        Was Uns Auszeichnet
+      </p>
+      <h3 className="xs:w-full font-cooper-hewitt xxxs:text-3xl xs:mx-0 mx-auto w-[226px] pb-8 text-2xl leading-tight font-semibold tracking-tight opacity-80 lg:pb-20 lg:text-[32px]">
+        Die Werte, die uns antreiben
+      </h3>
+      <div className="xs:grid-cols-[1fr_2fr] grid gap-5 lg:grid-cols-4">
+        <TallImageDrop className="xs:justify-self-end order-1 justify-self-center lg:order-0" src={image1} />
+        <TallTextBox
+          className="xs:[&>p]:block order-2 justify-self-center lg:-top-8 lg:order-0"
+          icon={icon1}
+          heading="Partnerschaft"
+          text="Langfristige Partner
 sind unser Fundament, getragen von offener Kommunikation und erstklassiger Reinigungsqualität."
-      />
+        />
 
-      <TallTextBox
-        className="xs:[&>p]:block order-4 justify-self-center lg:-top-8 lg:order-0"
-        icon={icon2}
-        heading="Zuverlässig"
-        text="Seit über 30 Jahren vereinen wir präzise Handwerkskunst mit moderner Effizienz und schaffen so nachhaltig beeindruckende Ergebnisse."
-      />
-      <TallImageDrop className="xs:justify-self-end order-3 justify-self-center lg:order-0" src={image3} />
-      <TallImageDrop className="xs:justify-self-end order-5 justify-self-center lg:order-0" src={image2} />
-      <TallTextBox
-        className="xs:[&>p]:block order-6 justify-self-center lg:-top-8 lg:order-0"
-        icon={icon3}
-        heading="Fortschrittlich"
-        text="Zukunftsweisende Technologien für beste Reinigungsergebnisse, maximale Effizienz und spürbare Qualität."
-      />
+        <TallTextBox
+          className="xs:[&>p]:block order-4 justify-self-center lg:-top-8 lg:order-0"
+          icon={icon2}
+          heading="Zuverlässig"
+          text="Seit über 30 Jahren vereinen wir präzise Handwerkskunst mit moderner Effizienz und schaffen so nachhaltig beeindruckende Ergebnisse."
+        />
+        <TallImageDrop className="xs:justify-self-end order-3 justify-self-center lg:order-0" src={image3} />
+        <TallImageDrop className="xs:justify-self-end order-5 justify-self-center lg:order-0" src={image2} />
+        <TallTextBox
+          className="xs:[&>p]:block order-6 justify-self-center lg:-top-8 lg:order-0"
+          icon={icon3}
+          heading="Fortschrittlich"
+          text="Zukunftsweisende Technologien für beste Reinigungsergebnisse, maximale Effizienz und spürbare Qualität."
+        />
 
-      <TallTextBox
-        className="xs:[&>p]:block order-8 justify-self-center lg:-top-8 lg:order-0"
-        icon={icon4}
-        heading="Expertengeführt"
-        text="Fokussierte Gebäudereinigung mit maßgeschneiderten Lösungen, die genau Ihren Anforderungen entsprechen."
-      />
-      <TallImageDrop className="xs:justify-self-end order-7 justify-self-center lg:order-0" src={image4} />
+        <TallTextBox
+          className="xs:[&>p]:block order-8 justify-self-center lg:-top-8 lg:order-0"
+          icon={icon4}
+          heading="Expertengeführt"
+          text="Fokussierte Gebäudereinigung mit maßgeschneiderten Lösungen, die genau Ihren Anforderungen entsprechen."
+        />
+        <TallImageDrop className="xs:justify-self-end order-7 justify-self-center lg:order-0" src={image4} />
+      </div>
     </motion.div>
   );
 }
