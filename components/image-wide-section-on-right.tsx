5cb924d435a266176c8ee8711f729c5195acdba7
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-17 10:52:15 +0100

    feat:ai:claude: spotted that the fix was self-start to override the default stretch of grid items

--- components/image-wide-section-on-right.tsx
+++ components/image-wide-section-on-right.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 
 export default function ImageWideSectionOnRight({ image }: { image: StaticImageData }) {
   return (
-    <div className="grid grid-cols-[31px_1fr]">
-      <div className="after:bg-theme-color-image-drop w-full] relative col-start-2 aspect-[calc(441/354)] h-auto w-full after:absolute after:top-1 after:left-1 after:-z-10 after:aspect-[calc(441/354)] after:h-auto after:w-full after:rounded-tl-[80px] after:rounded-br-[80px] after:content-['']">
+    <div className="grid grid-cols-[31px_1fr] self-start">
+      <div className="after:bg-theme-color-image-drop relative col-start-2 aspect-[calc(441/354)] h-auto w-full after:absolute after:top-1 after:left-1 after:-z-10 after:aspect-[calc(441/354)] after:h-auto after:w-full after:rounded-tl-[80px] after:rounded-br-[80px] after:content-['']">
         <div className="border-theme-color-image-border bg-theme-color-image-drop relative z-20 h-full w-full overflow-clip rounded-tl-[80px] rounded-br-[80px] border">
           <Image
             src={image}
             alt="image"
             className="bg-theme-color-image-drop h-full w-full object-cover"
-            // width={1764}
-            // height={1200}
             fill
             sizes="441px"
             quality={20}
--- components/utility-components/image-wide-section-on-left.tsx
+++ components/utility-components/image-wide-section-on-left.tsx
@@ -3,18 +3,13 @@ import React from 'react';
 
 export default function ImageWideSectionOnLeft({ image }: { image: StaticImageData }) {
   return (
-    <div className="relative grid grid-cols-[1fr_31px]">
-      <div className="cols-start-1 relative aspect-[calc(441/354)] h-auto w-full">
-        <div className="absolute m-0 h-full w-full p-0">
-          <div className="bg-theme-color-image-drop 2sm:mr-[31px] relative top-1 right-1 mr-0 aspect-[calc(441/354)] h-auto w-full rounded-tr-[80px] rounded-bl-[80px] p-0 sm:mr-3"></div>
-        </div>
+    <div className="grid grid-cols-[1fr_31px] self-start">
+      <div className="after:bg-theme-color-image-drop relative col-start-1 aspect-[calc(441/354)] h-auto w-full after:absolute after:top-1 after:right-1 after:-z-10 after:aspect-[calc(441/354)] after:h-auto after:w-full after:rounded-tr-[80px] after:rounded-bl-[80px] after:content-['']">
         <div className="border-theme-color-image-border relative z-20 h-full w-full overflow-clip rounded-tr-[80px] rounded-bl-[80px] border">
           <Image
             src={image}
             alt="image"
             className="bg-theme-color-image-drop h-full w-full object-cover"
-            // width={1764}
-            // height={1200}
             fill
             sizes="441px"
             quality={20}
