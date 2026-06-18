dc7b5dc681465493496e4c23c3d50cfdbcf833b4
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-18 12:53:35 +0100

    feat:ai:i: being able to descrbe the problem now that i understand it well i was able to one shot the fix with ai , which was basicaly resolving compeating layout instructions of h-full and so on

--- components/utility-components/double-image-wide-section-on-left.tsx
+++ components/utility-components/double-image-wide-section-on-left.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import image from '@/public/images/team-two-happy-green3.jpg';
-import Image, { StaticImageData } from 'next/image';
+import { StaticImageData } from 'next/image';
 import TallImageDrop from './image-tall-drop-left';
 
 export default function DoubleImageWideSectionOnLeft({
@@ -12,19 +11,14 @@ export default function DoubleImageWideSectionOnLeft({
 }) {
   return (
     <div className="2sm:grid-cols-[1fr_20px] grid grid-cols-[1fr_0px] self-start lg:grid-cols-[1fr_41px]">
-      <div className={`relative col-start-1 aspect-[calc(441/354)] h-auto w-full`}>
-        <div className="absolute top-0 right-0 bottom-0 left-0 grid h-full grid-cols-[55%_45%]">
-          <TallImageDrop src={image1} className="col-start-1 h-full w-full" />
-          {/* <div className="col-start-1 h-full w-full bg-black"></div> */}
-          {/* <div className="relative top-20 -left-3 z-20"> */}
-          <div className="absolute top-0 right-0 bottom-0 left-0 grid h-full grid-cols-[45%_55%] items-start">
-            <TallImageDrop src={image2} className="relative top-20 z-20 col-start-2 h-full w-full" />
-            {/* <div className="relative top-20 z-20 col-start-2 h-full w-full bg-green-800"></div> */}
+      <div className="relative col-start-1 aspect-[calc(441/354)] h-auto w-full overflow-visible">
+        <div className="absolute inset-0 grid grid-cols-[55%_45%] items-start">
+          <TallImageDrop src={image1} className="col-start-1 w-full" />
+          <div className="absolute inset-0 grid grid-cols-[45%_55%] items-start">
+            <TallImageDrop src={image2} className="z-20 col-start-2 w-full translate-y-[28%]" />
           </div>
-          {/* </div> */}
         </div>
       </div>
     </div>
   );
 }
-// aspect ration 226/285
--- components/utility-components/image-tall-drop-left.tsx
+++ components/utility-components/image-tall-drop-left.tsx
@@ -12,19 +12,18 @@ export default function TallImageDrop({
 }) {
   return (
     <div
-      className={`${className} after:bg-theme-color-image-drop relative aspect-[calc(226/285)] h-fit w-full after:absolute after:top-1 ${dropIsOnLeft ? 'after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'} after:-z-10 after:h-[285px] after:w-[226px] after:content-['']`}
+      className={`${className} after:bg-theme-color-image-drop relative aspect-[calc(226/285)] h-auto w-full after:absolute after:top-1 after:-z-10 after:aspect-[calc(226/285)] after:h-auto after:w-full after:content-[''] ${dropIsOnLeft ? 'after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'}`}
     >
       <div
-        className={`border-theme-color-image-border relative z-20 h-fit w-full overflow-clip ${dropIsOnLeft ? 'rounded-tr-[80px] rounded-bl-[80px]' : 'rounded-tl-[80px] rounded-br-[80px]'} border`}
+        className={`border-theme-color-image-border relative z-20 h-full w-full overflow-clip border ${dropIsOnLeft ? 'rounded-tr-[80px] rounded-bl-[80px]' : 'rounded-tl-[80px] rounded-br-[80px]'}`}
       >
         <Image
           src={src}
           alt="image of video"
           quality={20}
           placeholder="blur"
-          // width={904} height={1140}
           className="object-cover"
-          sizes="224px"
+          sizes="(max-width: 640px) 55vw, 280px"
           fill
         />
       </div>
