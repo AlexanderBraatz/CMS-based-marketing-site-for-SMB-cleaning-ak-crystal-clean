174222c309d232e7df044126eaa51a8f97c97456
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-06-18 17:44:08 +0100

    feat:ai: make hero image responsive too

--- components/utility-components/image-wide-home.tsx
+++ components/utility-components/image-wide-home.tsx
@@ -3,17 +3,15 @@ import Image, { StaticImageData } from 'next/image';
 
 export default function ImageWideHome({ image }: { image: StaticImageData }) {
   return (
-    <div className="flex justify-end">
-      <div className="after:bg-theme-color-image-drop relative h-[300px] w-[441px] after:absolute after:top-2 after:left-2 after:-z-10 after:h-[300px] after:w-[441px] after:rounded-tl-[80px] after:rounded-br-[80px] after:content-['']">
-        <div className="border-theme-color-image-border relative z-20 h-full w-full overflow-clip rounded-tl-[80px] rounded-br-[80px] border">
+    <div className="2sm:pl-[31px] flex justify-end pl-0 sm:pl-3">
+      <div className="after:bg-theme-color-image-drop relative aspect-[calc(441/300)] h-auto w-full after:absolute after:top-2 after:left-2 after:-z-10 after:aspect-[calc(441/300)] after:h-auto after:w-full after:rounded-tl-[80px] after:rounded-br-[80px] after:content-['']">
+        <div className="border-theme-color-image-border bg-theme-color-image-drop relative z-20 h-full overflow-clip rounded-tl-[80px] rounded-br-[80px] border">
           <Image
             src={image}
-            className=":bg-theme-color-image-drop h-full w-full object-cover"
+            className="bg-theme-color-image-drop h-full w-full object-cover"
             alt="image of video"
-            // width={1692}
-            // height={1091}
             fill
-            sizes="439px"
+            sizes="(max-width: 640px) 90vw, (max-width: 1024px) 45vw, 441px"
             quality={20}
             placeholder="blur"
           />
