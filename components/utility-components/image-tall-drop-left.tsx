d86188bf1b90cd0187aac150b1c8b14fa754a603
AlexanderBraatz <alex.braatz.g@gmail.com>
2026-07-10 17:34:19 +0100

    chore:ai: fix images shadow positioning on safari

--- components/utility-components/image-tall-drop-left.tsx
+++ components/utility-components/image-tall-drop-left.tsx
@@ -14,10 +14,10 @@ export default function TallImageDrop({
 }) {
   return (
     <div
-      className={`${className} after:bg-theme-color-image-drop relative aspect-[calc(226/285)] h-auto w-full after:absolute after:top-1 after:-z-10 after:aspect-[calc(226/285)] after:h-auto after:w-full after:content-[''] ${dropIsOnLeft ? 'after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'}`}
+      className={`${className} after:bg-theme-color-image-drop relative aspect-[calc(226/285)] h-auto w-full after:absolute after:top-1 after:-z-10 after:h-full after:w-full after:content-[''] ${dropIsOnLeft ? 'after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'}`}
     >
       <div
-        className={`border-theme-color-image-border relative z-20 h-full w-full overflow-clip border ${dropIsOnLeft ? 'rounded-tr-[80px] rounded-bl-[80px]' : 'rounded-tl-[80px] rounded-br-[80px]'}`}
+        className={`border-theme-color-image-border absolute inset-0 z-20 overflow-clip border ${dropIsOnLeft ? 'rounded-tr-[80px] rounded-bl-[80px]' : 'rounded-tl-[80px] rounded-br-[80px]'}`}
       >
         <Image
           src={src}
--- components/utility-components/image-wide-section.tsx
+++ components/utility-components/image-wide-section.tsx
@@ -22,7 +22,7 @@ export default function ImageWideSection({
       className={`grid self-start ${isOnLeft ? '2sm:grid-cols-[1fr_20px] grid-cols-[1fr_0px] lg:grid-cols-[1fr_41px]' : '2sm:grid-cols-[20px_1fr] grid-cols-[0px_1fr] lg:grid-cols-[41px_1fr]'} ${className}`}
     >
       <div
-        className={`after:bg-theme-color-image-drop relative aspect-[calc(441/354)] h-auto w-full after:absolute after:top-1 after:-z-10 after:aspect-[calc(441/354)] after:h-auto after:w-full after:content-[''] ${isOnLeft ? 'col-start-1 after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'col-start-2 after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'}`}
+        className={`after:bg-theme-color-image-drop relative aspect-[calc(441/354)] h-auto w-full after:absolute after:top-1 after:-z-10 after:h-full after:w-full after:content-[''] ${isOnLeft ? 'col-start-1 after:right-1 after:rounded-tr-[80px] after:rounded-bl-[80px]' : 'col-start-2 after:left-1 after:rounded-tl-[80px] after:rounded-br-[80px]'}`}
       >
         <div
           className={`border-theme-color-image-border relative z-20 h-full w-full overflow-clip border ${isOnLeft ? 'rounded-tr-[80px] rounded-bl-[80px]' : 'bg-theme-color-image-drop rounded-tl-[80px] rounded-br-[80px]'}`}
